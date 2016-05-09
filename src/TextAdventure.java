ecebc3192143d27601fd5b719fca2c938a17e3b0
Sarah Trop <sctrop@gmail.com>
2016-05-09 10:23:25 -0500

    First commit

---
+++ src/TextAdventure.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TextAdventure {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		boolean playing = true;
+		
+		while(playing) {
+			
+		}
+		
+	}
+}
